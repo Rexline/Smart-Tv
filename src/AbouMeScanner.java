d2194c45e3be39273a2baaf06a18c0b15071aa66
Rexline <103515200+Rexline@users.noreply.github.com>
2023-11-17 16:50:09 -0300

    Add class AboutMeScanner

---
+++ src/AbouMeScanner.java
@@ -0,0 +1,30 @@
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AbouMeScanner {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+
+
+//        imprimir os dados obtidos
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha autura é " + altura + "cm");
+
+    }
+}
