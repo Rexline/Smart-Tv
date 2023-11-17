d11cbce3c63b35e2b7ad91b2f7e536b73b202ea4
Rexline <103515200+Rexline@users.noreply.github.com>
2023-11-17 18:47:33 -0300

    Add try e catch

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -1,12 +1,21 @@
+import java.util.InputMismatchException;
+
 public class AboutMe {
     public static void main(String[] args) {
-        String nome = args [0];
-        String sobrenome = args [1];
-        Integer idade = Integer.valueOf(args[2]);
-        Double altura = Double.valueOf(args[3]);
+        try {
+            String nome = args[0];
+            String sobrenome = args[1];
+            Integer idade = Integer.valueOf(args[2]);
+            Double altura = Double.valueOf(args[3]);
 
-        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos");
-        System.out.println("Minha autura é " + altura + "cm");
+            System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade + " anos");
+
+            System.out.println("Minha autura é " + altura + "cm");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Os Campos idade e altura precisam ser numéricos.");
+        }
+
     }
 }
